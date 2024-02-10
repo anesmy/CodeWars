2ddc8780907707653766788e07415cae6a0e1cd1
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2024-02-10 13:10:02 +0200

    Greet Me

---
+++ src/main/java/org/example/seven/GreetMe.java
@@ -0,0 +1,10 @@
+package org.example.seven;
+
+// https://www.codewars.com/kata/535474308bb336c9980006f2/train/java
+public class GreetMe {
+    public static String greet(String name){
+        return "Hello "
+                + Character.toUpperCase(name.charAt(0))
+                + name.substring(1).toLowerCase()+ "!" ;
+    }
+}
